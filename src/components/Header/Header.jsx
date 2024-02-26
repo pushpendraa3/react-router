12b45e6ce5a039e1b30861bc5c14387ef290f52a
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-02-26 14:53:49 +0530

    set up router better way

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -58,7 +58,7 @@ export default function Header() {
                                         `${isActive ? "text-orange-700" : "text-gray-700"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
-                                    contact
+                                    Contact
                                 </NavLink>
                             </li>
 
